43535573ec8a33b4b33d791d0dacac025e6147e6
wtfychxx <fadhliyulyanto@gmail.com>
2021-06-10 11:43:04 +0700

    replace content without refreshing the page

---
+++ assets/js/loader.js
@@ -0,0 +1,40 @@
+const load = (e) => {
+    event.preventDefault();
+
+    if (typeof(e.target) == 'undefined') { return; }
+
+    if (e.target.tagName !== 'A') { return; }
+
+    const href = e.target.getAttribute('href');
+
+    const hrefParts = href.split('=');
+    const container = hrefParts[0].substr(1);
+    const source = hrefParts[1];
+
+    const request = new XMLHttpRequest();
+
+    request.onreadystatechange = () => {
+        if (request.readyState < 4) {
+            // handle preload
+            return;
+        }
+        if (request.status !== 200) {
+            // handle error http
+            return;
+        }
+        if (request.readyState === 4) {
+            successCallBack();
+        }
+    }
+
+    request.open('GET', source, true);
+    request.send('');
+
+    successCallBack = () => {
+        document.getElementById(container).innerHTML = request.responseText;
+
+        history.pushState('', '', `?${container}=${source}`);
+    }
+}
+
+document.addEventListener('click', load, false);
